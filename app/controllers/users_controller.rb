7f25e7150533d89adc6833572fb64f43f9b1f60d
Alex Howington <alex.howington@gmail.com>
2017-05-10 22:01:20 -0600

    Cache Lastfm and Discogs API results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   def show
     @username = params[:username] || "saorock"
-    lastfm = Lastfm.new(ENV["LAST_FM_API_KEY"], ENV["LAST_FM_API_SECRET"])
-    @top_albums = lastfm.user.get_top_albums(user: @username, period: "6month", limit: 10)
+    @top_albums = Rails.cache.fetch("#{@username}_top_albums", expires_in: 20.minutes) do
+      lastfm = Lastfm.new(ENV["LAST_FM_API_KEY"], ENV["LAST_FM_API_SECRET"])
+      lastfm.user.get_top_albums(user: @username, period: "6month", limit: 10)
+    end
 
     @discogs_results = {}
     auth_wrapper = Discogs::Wrapper.new("Lastfm to Discogs Wantlist", user_token: ENV["DISCOGS_TOKEN"])
     @top_albums.each do |album|
-      search = auth_wrapper.search("#{album["name"]} #{album["artist"]["name"]}", per_page: 20, type: :release)
-      @discogs_results[album["name"]] = search.results
+      @discogs_results[album["name"]] = Rails.cache.fetch("#{album["name"]}_#{album["artist"]["name"]}", expires_in: 20.minutes) do
+        search = auth_wrapper.search("#{album["name"]} #{album["artist"]["name"]}", per_page: 20, type: :release)
+        search.results
+      end 
     end
   end
 end
