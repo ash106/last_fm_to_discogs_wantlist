5c64c85338ff844cef4eec832a8a89a1431442b2
Alex Howington <alex.howington@gmail.com>
2017-05-10 20:09:45 -0600

    Use Discogs API to search for Lastfm album results

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'lastfm', '~> 1.27', '>= 1.27.1'
+gem "discogs-wrapper"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
     @username = params[:username] || "saorock"
     lastfm = Lastfm.new(ENV["LAST_FM_API_KEY"], ENV["LAST_FM_API_SECRET"])
     @top_albums = lastfm.user.get_top_albums(user: @username, period: "6month", limit: 10)
+
+    @discogs_results = {}
+    auth_wrapper = Discogs::Wrapper.new("Lastfm to Discogs Wantlist", user_token: ENV["DISCOGS_TOKEN"])
+    @top_albums.each do |album|
+      search = auth_wrapper.search("#{album["name"]} #{album["artist"]["name"]}", per_page: 20, type: :release)
+      @discogs_results[album["name"]] = search.results
+    end
   end
 end
